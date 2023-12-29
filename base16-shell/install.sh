60e6efce2bf13d3ec7614c8194e0b37358b1cf5b
JefferyLim <jeffery.lim@fastmail.com>
2023-12-29 06:08:54 +0000

    Adding base16-shell
    - Adding install script for base16-shell
    - Updating tmux status bar to match base16 theme
    - Updated vimrc to match base16 theme

---
+++ base16-shell/install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+mkdir -p ~/.config/
+
+if [ -d ~/.config/bash16-shell ];then
+    git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
+fi
+
+command='# Base16 Shell
+BASE16_SHELL="$HOME/.config/base16-shell/"
+[ -n "$PS1" ] && \
+    [ -s "$BASE16_SHELL/profile_helper.sh" ] && \
+        source "$BASE16_SHELL/profile_helper.sh"
+        
+base16_tomorrow-night'
+
+grep -Fq "$command" ~/.bashrc || echo "$command" >> ~/.bashrc 
+
+source ~/.bashrc
