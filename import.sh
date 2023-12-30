21ae6c72e39bc9b622ba35e274b18b75abbf7d28
JefferyLim <jeffery.lim@fastmail.com>
2023-12-30 17:23:48 -0500

    Fixing path issues
    - Fixing path issue with symbolic links
    - Adding support for base16-vim

--- import.sh
+++ import.sh
@@ -10,7 +10,7 @@ for d in config/*; do
         if [ "$f" ]; then
             source "$f"
         else
-            ln -sf "$d" ~/.config/$(basename -- $d)
+            ln -sf "$PWD/$d" ~/.config/$(basename -- $d)
         fi
         echo "$d"
     fi
@@ -19,4 +19,8 @@ done
 # Custom shell commands for each dotfiles
 
 ## vim
-ln -sf dotfiles/vim/vimrc ~/.vimrc
+mkdir -p ~/.vim/colors
+git clone https://github.com/chriskempson/base16-vim.git ~/.vim/colors/base16
+cp ~/.vim/colors/base16/colors/*.vim ~/.vim/colors/.
+ln -sf "$PWD/dotfiles/vim/vimrc" ~/.vimrc
+
