31af8d4ec60fd6607c58656f639920549459c088
JefferyLim <jl+git@jefferyjlim.com>
2025-02-05 15:55:08 -0500

    Adding .profile import
    - Make .profile run bash

--- import.sh
+++ import.sh
@@ -17,7 +17,26 @@ for d in config/*; do
 done
 
 # Custom shell commands for each dotfiles
-## vim
+## .profile
+BLOCK='
+if [ -n "$BASH_VERSION" ]; then
+    # include .bashrc if it exists
+    if [ -f "$HOME/.bashrc" ]; then
+        . "$HOME/.bashrc"
+    fi
+fi
+'
+
+PROFILE="$HOME/.profile"
+# Check if the block already exists in .profile
+if ! grep -Fxq "$BLOCK" "$PROFILE"; then
+    echo "Adding the block to $PROFILE"
+    echo "$BLOCK" >> "$PROFILE"
+else
+    echo "Block already exists in $PROFILE"
+fi
+
+## .vim
 mkdir -p ~/.vim/colors
 git clone https://github.com/chriskempson/base16-vim.git ~/.vim/colors/base16
 cp ~/.vim/colors/base16/colors/*.vim ~/.vim/colors/.
